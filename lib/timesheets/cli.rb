eb5e27470a298360034c81ab723d530cb02a3cbd
Bradley J. Spaulding <brad.spaulding@gmail.com>
2014-05-04 13:53:29 -0500

    Alphabetized methods in CLI

--- lib/timesheets/cli.rb
+++ lib/timesheets/cli.rb
@@ -3,15 +3,15 @@ require 'terminal-table'
 require 'csv'
 
 require "timesheets/commands/base"
-require "timesheets/commands/summary"
-require "timesheets/commands/start"
 require "timesheets/commands/edit"
+require "timesheets/commands/start"
+require "timesheets/commands/summary"
 
 module Timesheets
   class CLI < Thor
-    desc "summary", "See a summary table of time worked"
-    def summary
-      Commands::Summary.run
+    desc "edit", "Manually edit timesheet data"
+    def edit
+      Commands::Edit.run
     end
 
     desc "start", "Begin a work session"
@@ -19,9 +19,9 @@ module Timesheets
       Commands::Start.run
     end
 
-    desc "edit", "Manually edit timesheet data"
-    def edit
-      Commands::Edit.run
+    desc "summary", "See a summary table of time worked"
+    def summary
+      Commands::Summary.run
     end
   end
 end
