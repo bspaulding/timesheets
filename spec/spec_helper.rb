670d73e7e3e55cb58988acf5964531f4120ed3e8
Bradley J. Spaulding <brad.spaulding@gmail.com>
2014-05-03 22:38:14 -0500

    Added test file setup to spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,3 +4,7 @@ SimpleCov.start
 require 'timesheets'
 require 'minitest/autorun'
 
+TIMESHEETS_FILEPATH='./tmp/timesheets.csv'
+FileUtils.mkdir_p('./tmp')
+File.open(TIMESHEETS_FILEPATH, 'w+') {|f| f.write('Start,End') }
+
